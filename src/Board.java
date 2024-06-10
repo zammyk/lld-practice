e9dc3bfae4c2f5fa10aed647ab500f71668b54ff
Rahul Hembrom <rhembrom21@gmail.com>
2024-06-10 18:12:38 +0530

    Add board logic

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,37 @@
 import symbols.Symbol;
 
+import java.util.Objects;
+
 public class Board {
     int size;
-    private Symbol[][] board;
+    private final Symbol[][] board;
     Board (int size) {
+        this.size = size;
         this.board = new Symbol[size][size];
+    }
+    boolean isCellEmpty (int row, int col) {
+        return Objects.isNull(board[row][col]);
+    }
+    void play (Symbol symbol, int row, int col) {
+        board[row][col] = symbol;
+    }
+    boolean isGameComplete () {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (Objects.isNull(board[row][col])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean winnerFound() {
+        // TODO: add winnerFound logic
+        return true;
+    }
+
+    public void display() {
+        // TODO: add display logic
     }
 }
