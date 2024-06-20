532495fe9717ce45d67303628e0f6f29f0fbed1c
Rahul Hembrom <rhembrom21@gmail.com>
2024-06-20 20:02:58 +0530

    Add methods to exit and entry terminals

--- src/parking_lot/terminals/EntryTerminal.java
+++ src/parking_lot/terminals/EntryTerminal.java
@@ -2,6 +2,7 @@ package parking_lot.terminals;
 
 import parking_lot.Ticket;
 import parking_lot.assignment_strategy.ParkingSpotAssignmentStrategy;
+import parking_lot.parkingspots.ParkingSpot;
 import parking_lot.parkingspots.ParkingSpotType;
 
 public class EntryTerminal extends Terminal {
@@ -9,8 +10,8 @@ public class EntryTerminal extends Terminal {
     EntryTerminal (ParkingSpotAssignmentStrategy parkingSpotAssignmentStrategy) {
         this.parkingSpotAssignmentStrategy = parkingSpotAssignmentStrategy;
     }
-    Ticket generateTicket(ParkingSpotType parkingSpotType) {
-        int parkingSpotId = parkingSpotAssignmentStrategy.getParkingSpot(parkingSpotType, this.id);
-        return new Ticket(parkingSpotId, parkingSpotType);
+    public Ticket generateTicket (ParkingSpotType parkingSpotType) {
+        ParkingSpot parkingSpot = parkingSpotAssignmentStrategy.getParkingSpot(parkingSpotType, this.id);
+        return new Ticket(parkingSpot.getId(), parkingSpotType);
     }
 }
--- src/parking_lot/terminals/ExitTerminal.java
+++ src/parking_lot/terminals/ExitTerminal.java
@@ -1,12 +1,17 @@
 package parking_lot.terminals;
 
 import parking_lot.Ticket;
+import parking_lot.parkingspots.ParkingRepository;
+import parking_lot.parkingspots.ParkingSpot;
 
 import java.time.Instant;
 import java.time.temporal.TemporalField;
 
 public class ExitTerminal extends Terminal {
+    ParkingRepository parkingRepository;
     public double getTicketFees(Ticket ticket) {
+        ParkingSpot parkingSpot = parkingRepository.getParkingSpotFromId(ticket.getParkingSpotId());
+        parkingSpot.free();
         return (Instant.now().getNano() - ticket.getEntranceTime().getNano()) * ticket.getParkingSpotType().cost;
     }
 }
